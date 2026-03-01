dc02d6e299e8f1f83ac8bc921af44848dcf1b3cf
sharanya <sharanyakulal17@gmail.com>
2026-03-01 16:25:56 +0530

    Delete server directory

--- server/models/job.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const jobSchema = new mongoose.Schema({
-  title: String,
-  company: String,
-  location: String,
-});
-
-module.exports = mongoose.model("Job", jobSchema);
